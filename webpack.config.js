ec8d31d7e50d0182d5c2a2b9c418f4b9fede2c26
Muhammad Ashraf <muhammad.a.hassan.0@gmail.com>
2022-03-12 23:13:10 +0200

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require("path")
+const htmlPlugin = require("html-webpack-plugin")
+const cssExtractPlugin = require('mini-css-extract-plugin');
+const cssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const imagesPlugin = require('image-minimizer-webpack-plugin');
+
+module.exports = {
+  mode: "production",
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.min.js",
+    path: path.resolve(__dirname, "dist"),
+    assetModuleFilename: 'images/[name][ext]'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [cssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          cssExtractPlugin.loader,
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ]
+  },
+  plugins: [
+    new htmlPlugin({ template: "src/index.html" }),
+    new cssExtractPlugin({ filename: "styles.css" }),
+    new CleanWebpackPlugin(),
+    new cssMinimizerPlugin()
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [
+      "...",
+      new imagesPlugin({
+        minimizerOptions: {
+          plugins: [
+            ["gifsicle", { interlaced: true }],
+            ["mozjpeg", { quality: 70 }],
+            ["optipng", { optimizationLevel: 5 }],
+            ['svgo']
+          ],
+        },
+      }),
+    ]
+  }
+}
